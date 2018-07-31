9c5b41366cbe0e702bf7551987cb5c1aec2f7613
jeremy lanfranchi <lanfranchi.jeremy@gmail.com>
2018-07-31 16:06:00 +0800

    All the back end is working, youhouuu

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
 
-  validates_uniqueness_of :ingredient
+  validates_uniqueness_of :ingredient, scope: :cocktail
 end
